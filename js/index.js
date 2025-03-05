aa59f2915488968ba9186244c59ca223960a7acc
DEEPAK-IAS <deepaksurya2002dgl@gmail.com>
2025-03-05 16:19:46 +0530

    welcome

---
+++ js/index.js
@@ -0,0 +1,29 @@
+
+// const user = ['surya', 'deepak', 1, [2,3,4]]
+
+// console.log(user[3][0]);
+
+
+// const users  = [
+//   {id: 1, name: "ajith"},
+//   {id: 2, name: "surya"},
+//   {id: 3, name: "nidish"},
+//   {id: 4, name: "robert"},
+//   {id: 5, name: "ranjith"},
+// ]
+
+// console.log(users[0].name)
+
+/*
+const date = new Date();
+console.log(date);
+console.log(date.getFullYear());
+console.log(date.getMonth());
+*/
+
+
+age = 18
+
+const result = (age >= 18) ? (age == 18) ? "next year you will be vote": "eligible" : "not";
+
+console.log(result)
